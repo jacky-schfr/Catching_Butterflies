ae67ef6fce6496c2cfda1e18133532f4be05bf67
Jacqueline S <101556121+jacky-schfr@users.noreply.github.com>
2022-06-07 15:25:06 +0200

    bugs are moving.

--- src/main/java/com/schfr/catchingbutterflies/Bug.java
+++ src/main/java/com/schfr/catchingbutterflies/Bug.java
@@ -1,11 +1,10 @@
 package com.schfr.catchingbutterflies;
 
 public class Bug {
-    int flyingSpeed, xPos, yPos;
+    int flyingSpeed, xPos, yPos, randomPos;
 
     public Bug(int x, int y){
         xPos = x;
         yPos = y;
     }
-
 }
--- src/main/java/com/schfr/catchingbutterflies/Controller.java
+++ src/main/java/com/schfr/catchingbutterflies/Controller.java
@@ -16,58 +16,77 @@ public class Controller {
     public Arc netShape;
     int bugFrequency;
 
-    AnimationTimer bugTimer = null;
 
     Random rand = new Random();
 
     Bug[] bugList;
-    BugHunter hunter =  new BugHunter(100, 100);
+    BugHunter hunter = new BugHunter(100, 100);
     public Canvas canvas;
     public GraphicsContext gContex;
 
-    public void start(){
+    public void start() {
         gContex = canvas.getGraphicsContext2D();
         bugFrequency = 3;
         bugs();
     }
-    public void update(){
-        for(Bug b :bugList){
+
+    public void update() {
+        gContex.clearRect(0, 0, canvas.getHeight(), canvas.getWidth());
+        for (Bug b : bugList) {
             gContex.fillOval(b.xPos, b.yPos, Var.bugSize, Var.bugSize);
+            if (b.randomPos == 0) {
+                if (b.xPos <= canvas.getHeight()) {
+                    b.xPos += 1;
+                }
+            }
+            if (b.randomPos == 1) {
+                if (b.xPos >= -Var.bugSize) {
+                    b.xPos -= 1;
+                }
+            }
+            if (b.randomPos == 2) {
+                if (b.yPos <= canvas.getHeight()) {
+                    b.yPos += 1;
+                }
+            }
+            if (b.randomPos == 3) {
+                if (b.yPos >= -Var.bugSize) {
+                    b.yPos -= 1;
+                }
+            }
         }
         hunter.lvl += 1;
         bugFrequency += 2;
-        System.out.println(Arrays.toString(bugList));
     }
 
-    public void bugs(){
-        Var.bugX = 0;
-        Var.bugY = 0;
+    public void bugs() {
         bugList = new Bug[bugFrequency];
-        for(int  i = 0; i<bugFrequency; i++){
-            startSide();
-         bugList[i] = new Bug(Var.bugX, Var.bugY);
+        for (int i = 0; i < bugFrequency; i++) {
+            bugPosition(i);
         }
     }
-    public void startSide(){
+
+    public void bugPosition(int i) {
         int boundSize = (int) (canvas.getHeight() - Var.bugSize);
         int sideValue = rand.nextInt(4);
-        System.out.println(sideValue);
 
-        if(sideValue == 0){
+        if (sideValue == 0) {
             Var.bugX = 0;
             Var.bugY = rand.nextInt(boundSize);
         }
-        if(sideValue == 1){
+        if (sideValue == 1) {
             Var.bugX = boundSize;
-            Var.bugY= rand.nextInt(boundSize);
+            Var.bugY = rand.nextInt(boundSize);
         }
-        if (sideValue == 2){
+        if (sideValue == 2) {
             Var.bugY = 0;
             Var.bugX = rand.nextInt(boundSize);
         }
-        if (sideValue == 3){
+        if (sideValue == 3) {
             Var.bugY = boundSize;
             Var.bugX = rand.nextInt(boundSize);
         }
+        bugList[i] = new Bug(Var.bugX, Var.bugY);
+        bugList[i].randomPos = sideValue;
     }
 }
